7f236757754ac5409ac84dcd64aad22ccf5327e2
Tom Winch <tom.winch@ofcom.org.uk>
2017-09-13 11:35:21 +0100

    Add python pip installer for sdrplay module

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # SDRplaySpecAn
 Grab IQ data from the SDRplay, and then pass it through the Raspberry Pi GPU FFT to get a spectrum 
-This project is an amalgamation of the SDRplay example code that produces IQ data, with the callback modified to only pass daya when the FFT is ready. There is some windowing code, and then the GPU_FFT code from the Raspberry Pi Project. 
+This project is an amalgamation of the SDRplay example code that produces IQ data, with the callback modified to only pass daya when the FFT is ready. There is some windowing code, and then the GPU_FFT code from the Raspberry Pi Project.
+
+To install the python package 'sdrplay':
+
+0. Install the MIR SDR play libraries and headers
+1. Build the library and install (sudo make install)
+2. Install the Python package using pip (sudo -H pip install -e .)
+ 
---
+++ setup.py
@@ -0,0 +1,105 @@
+""" A setuptools based setup module.
+
+    See:
+    https://packaging.python.org/en/latest/distributing.html
+    https://github.com/pypa/sampleproject
+"""
+from setuptools import setup, find_packages
+import subprocess
+import os
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='sdrplay',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=subprocess.check_output(['git', 'describe', '--tags']).decode().replace('-', '_').strip(),
+
+    description='SDRplaySpecAn python package',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url=subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']),
+
+    # Author details
+    author='Jenny Bailey and Tom Winch',
+    author_email='jenny.bailey@ofcom.org.uk',
+
+    # Choose your license
+    license='GPL',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        #'Intended Audience :: End Users/Desktop',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    # What does your project relate to?
+    keywords='sdrplay spectrum analysis',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # packages=['sdrplay'],
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    py_modules=["sdrplay"],
+
+    # There are optimizations in Python eggs that allows it to read .py files
+    # even from a compressed format (think of Java's JAR format), but since we
+    # have data files ( HTML and CSS again ) we need to tell setuptools to turn
+    # off this behavior with zip_safe=False
+    zip_safe=False,
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    #extras_require={
+    #    'dev': ['check-manifest'],
+    #    'test': ['coverage'],
+    #},
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    #data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+    },
+)
